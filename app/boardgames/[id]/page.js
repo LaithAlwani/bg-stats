55c6fd754d53973f4f2f624b0abc2bddcb04cd40
laith <laithalwani@gmail.com>
2024-08-03 17:08:09 -0400

    adds boardgame page

--- app/boardgames/[id]/page.js
+++ app/boardgames/[id]/page.js
@@ -1,7 +1,32 @@
-import React from 'react'
+"use client";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function BoardgamePage() {
+  const params = useParams();
+  const [boardgame, setBoardgame] = useState({});
+  const getBoardgame = async () => {
+    const res = await fetch(`/api/boardgames/${params.id}`);
+    if (res.ok) {
+      const data = await res.json();
+      console.log(data);
+      setBoardgame(data);
+    }
+  };
+  useEffect(() => {
+    getBoardgame();
+  }, []);
   return (
-    <div>BoardgamePage</div>
-  )
+    <div>
+      {boardgame && (
+        <>
+          <h2>{boardgame.title}</h2>
+          <img src={boardgame.image} />
+          <span>
+            {boardgame.minPlayers} - {boardgame.maxPlayers}
+          </span>
+        </>
+      )}
+    </div>
+  );
 }
